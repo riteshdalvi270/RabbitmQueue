13da8ad1a5e65cb8afd43096800d06133efc5a7f
Ritesh Dalvi <riteshdalvi270@gmail.com>
2018-02-18 22:59:16 -0800

    RabbitMQ receiver coded.

---
+++ src/main/java/com/messagequeue/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,27 @@
+package com.messagequeue.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RabbitmqApplication {
+
+	final static String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("Spring boot exchange");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqApplication.class, args);
+	}
+}
